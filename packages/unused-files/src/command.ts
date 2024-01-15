2e19087a70aa894e31cff21fdb9d3e816adb669b
Noah <noahnu@gmail.com>
2024-01-14 23:25:46 -0500

    feat: initial release of @noahnu/unused-files (#1)

---
+++ packages/unused-files/src/command.ts
@@ -0,0 +1,46 @@
+import { Command, Option } from 'clipanion'
+import * as t from 'typanion'
+
+import { findUnusedFiles } from './api'
+
+export class BaseCommand extends Command {
+    entryFiles = Option.Array('--entry', {
+        description:
+            'Entry files into the codebase. These files are known to be ' +
+            'used and any files that are dependencies of these entry files ' +
+            'are also considered used files.',
+        required: true,
+    })
+
+    ignorePatterns = Option.Array('--ignore', {
+        description:
+            'Glob patterns usued to exclude files. ' +
+            'The patterns are applied during traversal ' +
+            'of the directory tree.',
+        required: false,
+    })
+
+    depth = Option.String('--depth', {
+        description: 'Depth limit. Set to -1 to disable.',
+        required: false,
+        validator: t.isNumber(),
+    })
+
+    sourceDirectories = Option.Rest()
+
+    async execute(): Promise<number | void> {
+        const result = await findUnusedFiles({
+            entryFiles: this.entryFiles,
+            ignorePatterns: this.ignorePatterns,
+            sourceDirectories: this.sourceDirectories.length
+                ? this.sourceDirectories
+                : [process.cwd()],
+            depth:
+                typeof this.depth === 'undefined'
+                    ? -1
+                    : parseInt(Math.max(this.depth, -1).toFixed(0), 10),
+        })
+
+        this.context.stdout.write(result.unusedFiles.join('\n'))
+    }
+}
